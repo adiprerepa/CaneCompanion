1176a2c298e994c580df24605dc2dc49b17f6d56
Ishan Jain <ishanjain42@gmail.com>
2019-04-30 19:19:48 -0700

    ishan

---
+++ CaneCompanion/New Group/CreateAccount.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaneCompanion
+//
+//  Created by Ishan Jain on 4/28/19.
+//  Copyright © 2019 CaneCompanion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CaneCompanion/New Group/SignIn.swift
@@ -0,0 +1,30 @@
+//
+//  SignIn.swift
+//  CaneCompanion
+//
+//  Created by Ishan Jain on 4/28/19.
+//  Copyright © 2019 CaneCompanion. All rights reserved.
+//
+
+import UIKit
+
+class SignIn: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
