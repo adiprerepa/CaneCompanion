d8db00ebb4f1fc3cb25998b14518427adbd26b48
Aditya Prerepa <adiprerepa@gmail.com>
2019-05-04 18:32:45 +0000

    Removed Unnecesary files

--- test2/target/generated-sources/protobuf/grpc-java/io/grpc/examples/helloworld/GreeterGrpc.java
+++ test2/target/generated-sources/protobuf/grpc-java/io/grpc/examples/helloworld/GreeterGrpc.java
@@ -22,7 +22,7 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.20.0)",
-    comments = "Source: g.proto")
+    comments = "Source: greet.proto")
 public final class GreeterGrpc {
 
   private GreeterGrpc() {}
