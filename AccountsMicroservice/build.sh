cd0475b124406f4a25e35fe3eaf0a995fec71493
Aditya <adiprerepa@gmail.com>
2019-05-09 19:08:51 -0700

    removed some stuff

--- AccountsMicroservice/build.sh
+++
@@ -1,4 +0,0 @@
-mvn package;
-cd target;
-java -cp AccountsMicroservice-1.0-SNAPSHOT.jar:mysql-connector-java-8.0.15.jar com.prerepa.accounts.Main;
- 
--- AccountsMicroservice/runCron.sh
+++
@@ -1,2 +0,0 @@
-nohup java -cp target/AccountsMicroservice-1.0-SNAPSHOT.jar:target/mysql-connector-java-8.0.15.jar com.prerepa.accounts.Main &
-
