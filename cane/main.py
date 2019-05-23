3b088bb03ed26f3046ba8fed873d11335ae29ebc
Aditya Prerepa <adiprerepa@gmail.com>
2019-05-23 23:19:24 +0000

    from rpi

--- cane/main.py
+++ cane/main.py
@@ -1,69 +1,67 @@
 import geocoder
 import grpc
-import gps_pb2, gps_pb2_grpc, vision_pb2, vision_pb2_grpc
+import vision_pb2, vision_pb2_grpc
 from RPi import GPIO
 import os
 from picamera import PiCamera
 from time import sleep
-from io import BytesIO
+import time
 import io
 from multiprocessing import Process
+import traceback
 
 butpin = 2
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(butpin, GPIO.IN)
 
 ip = "canecompaniont420.ddns.net"
+myusername = "aditya"
 
+print("initializing")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(butpin, GPIO.IN)
 vision_channel = grpc.insecure_channel(ip + ":50055")
 vision_stub = vision_pb2_grpc.VisionMicroserviceStub(vision_channel)
-gps_channel = grpc.insecure_channel(ip + ":11612")
-gps_stub = gps_pb2_grpc.GpsServiceStub(gps_channel)
-
 camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 80
 sleep(2)
-print("Ready")
+print("ready")
 
-def keypressed():
+
+def key_pressed():
     return not GPIO.input(butpin)
 
-def caneCall(username, image_content):
-    return
-def coordThread():
-        while True:
-            print("send location")
-            latitude, longitude = geocoder.ip("me").latlng
-            print(latitude, longitude)
-            gps_stub.CaneInsert(gps_pb2.CaneCoordinates(username="aditya", latitude=latitude, longitude=longitude))
-            sleep(60)
+def camera_capture():
+    stream = io.BytesIO()
+    camera.capture(stream, "png")
+    return stream.getvalue()
 
-lastState = False
+def cane_call(image_content):
+    return vision_stub.CaneCall(vision_pb2.CaneRequest(username=myusername, png_data=image_content))
+
+last_state = False
 
 try:
-    thread = Process(target=coordThread, args=())
-    thread.start()
-
     while True:
-        curState = keypressed()
+        cur_state = key_pressed()
 
-        if (lastState != curState):
-            lastState = curState
+        if (last_state != cur_state):
+            last_state = cur_state
 
-            if (curState):
+            if (cur_state):
                 print("pressed")
-                stream = BytesIO()
                 print("taking pic")
-                camera.capture("image.png")
+                stream = camera_capture()
                 print("taken")
                 print("calling")
-                response = vision_stub.CaneCall(vision_pb2.CaneRequest(username="aditya", png_data=io.open("image.png", "rb").read()))
+                response = cane_call(stream)
                 print("called")
                 rfile = io.open("response.mp3", "wb")
                 rfile.write(response.wav_data)
                 rfile.close()
+                print("saved response")
                 os.system("omxplayer -o local response.mp3")
-
 except:
+    traceback.print_exc()
+    print("closing")
     camera.close()
-    thread.terminate()
     GPIO.cleanup()
